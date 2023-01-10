37c9e190d82e8024a5468512a30955b1408c359a
Daniel Won <100653363+brownshit@users.noreply.github.com>
2023-01-11 01:52:02 +0900

    Update ary_rev.c

--- ch02/ary_rev.c
+++ ch02/ary_rev.c
@@ -5,7 +5,8 @@
 #define CRT_SECURE_NO_WARNINGS
 //#pragma warning(disable:4996)
 /*===x,y교환하는 매크로함수===*/
-#define swap(type,x,y) do{type t = x; x = y; y = t; }while(0);//{type t = x; x = y; y = t;}
+#define swap(type,x,y) do{type t = x; x = y; y = t; }while(0)//{type t = x; x = y; y = t;}		
+//여기서는 while뒤에 세미콜론 없다. 아래에서 함수 호출시 세미콜론이 있기 때문. 매크로 함수는 그대로 들어간다고 생각하고 이상하면 오류가 나는것.
 
 void arr_inverse(int a[], int n) {// this func change arr, so have not define arr to const!
 	for (int i = 0; i < n/2; i++)		//해당 for 문은 n/2(둘다 정수라 몪만 반환된다.)만큼만 계산이 되어야 한다. n이 되면 원위치된다.
