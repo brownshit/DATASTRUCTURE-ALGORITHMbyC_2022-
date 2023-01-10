41f8b95bfddf4560134859c94890fca5cc8ace82
Daniel Won <100653363+brownshit@users.noreply.github.com>
2023-01-11 01:49:30 +0900

    Update ary_rev.c

--- ch02/ary_rev.c
+++ ch02/ary_rev.c
@@ -5,7 +5,7 @@
 #define CRT_SECURE_NO_WARNINGS
 //#pragma warning(disable:4996)
 /*===x,y교환하는 매크로함수===*/
-#define swap(type,x,y) {type t = x; x = y; y = t;}//do{type t = x; x = y; y = t; }while(0);
+#define swap(type,x,y) do{type t = x; x = y; y = t; }while(0);//{type t = x; x = y; y = t;}
 
 void arr_inverse(int a[], int n) {// this func change arr, so have not define arr to const!
 	for (int i = 0; i < n/2; i++)		//해당 for 문은 n/2(둘다 정수라 몪만 반환된다.)만큼만 계산이 되어야 한다. n이 되면 원위치된다.
